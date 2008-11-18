a24d8f7d0df540265ef6eca7768e40429f7766fe
Peter Marklund <peter_marklund@fastmail.fm>
2008-11-18 15:09:52 +0100

    Added before_destroy destroy all callbacks for associated objects in PageText and PageTextTranslation models. This is to avoid foreign key constraint violations on destroy.

--- lib/page_text.rb
+++ lib/page_text.rb
@@ -3,8 +3,15 @@ class PageText < ActiveRecord::Base
   validates_uniqueness_of :key
   
   translates :body
+
+  before_destroy :destroy_all_translations
   
   def translation
     globalize_translations.find_by_locale(I18n.locale)
   end
+  
+  private
+    def destroy_all_translations
+      globalize_translations.destroy_all
+    end
 end
--- lib/page_text_translation.rb
+++ lib/page_text_translation.rb
@@ -5,4 +5,11 @@ class PageTextTranslation < ActiveRecord::Base
   validates_presence_of :body
 
   acts_as_versioned :non_versioned_columns => [:page_text_id, :locale]
+
+  before_destroy :destroy_all_versions
+  
+  private
+    def destroy_all_versions
+      versions.destroy_all
+    end
 end
